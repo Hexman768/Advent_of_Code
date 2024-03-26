e15e86fd79fd7d596ab15ea3f9888e46312002f4
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2024-03-26 15:45:09 -0500

    Added solution for day 4 of 2015 Advent of Code  (#5)
    * UNSTABLE: Added changes
    
    * Fixed script and solved problem

---
+++ 2015/day_4/main.py
@@ -0,0 +1,30 @@
+import sys
+import hashlib
+
+def validate_hash(md5: str, zrs: int) -> bool:
+    for i in range(zrs):
+        if md5[i] != '0':
+            return False
+    return True
+
+def generate_md5(msg: str, zrs: int) -> str:
+    _c_value: int = 0
+    _md5: str = hashlib.md5(msg.encode('utf-8')).hexdigest()
+    _msg_cpy: str = msg
+    while not validate_hash(_md5, zrs):
+        _c_value += 1
+        msg = _msg_cpy + str(_c_value)
+        _md5 = hashlib.md5(msg.encode('utf-8')).hexdigest()
+    print('The integer required is:', _c_value)
+    return _md5
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print('ERROR - MISSING COMMAND LINE ARGUMENT: Input msg key for MD5 encyrption')
+        return
+    _msg: str = sys.argv[1]
+    print('Part 1:', generate_md5(_msg, 5))
+    print('Part 2:', generate_md5(_msg, 6))
+
+main()
