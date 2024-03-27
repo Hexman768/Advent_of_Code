61ca1a59cae9c8948b36cdb4a3172b9ba88a14a7
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-03-27 14:24:53 -0500

    Solved first part

--- 2015/day_5/main.py
+++ 2015/day_5/main.py
@@ -1,5 +1,18 @@
 import sys
 
+def contains_banned_strs(l: str) -> bool:
+    _b_str_lst = ['ab', 'cd', 'pq', 'xy']
+    for i in range(len(l) - 1):
+        if _b_str_lst.__contains__(l[i] + l[i + 1]): return True
+    return False
+
+def repeat_letter(l: str) -> bool:
+    _ch: str = ''
+    for i in range(len(l)):
+        if _ch == l[i]: return True
+        else: _ch = l[i]
+    return False
+
 def contains_vowel(l: str) -> bool:
     _c: int = 0
     _v_lst: list[str] = ['a', 'e', 'i', 'o', 'u']
@@ -9,14 +22,22 @@ def contains_vowel(l: str) -> bool:
     return True if _c >= 3 else False
 
 def main() -> None:
+    _c_value: int = 0
+    _b_str_lst: list[str] = ['ab', 'cd', 'pq', 'xy']
     if len(sys.argv) < 2:
         print("ERROR -> MISSING INPUT ARGUMENT!")
         return
     _fn: str = sys.argv[1]
     with open(_fn) as _file:
         for line in _file.readlines():
-            if contains_vowel(line): print('Contains at least three vowels')
-            else: print('Doesn\'t contain at least three vowels')
+            if contains_banned_strs(line):
+                print('Contains banned strings')
+            if contains_vowel(line) and repeat_letter(line) and not contains_banned_strs(line):
+                print(line, ': nice string')
+                _c_value += 1
+            else:
+                print(line, ': naughty string')
         _file.close()
+    print('There are', _c_value, 'nice strings')
 
 main()
