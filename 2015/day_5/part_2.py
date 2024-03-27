bfb991e4f067e5dfd06acec571a6bc5a48381d52
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-03-27 16:10:53 -0500

    Added changes to part 2 script

--- 2015/day_5/part_2.py
+++ 2015/day_5/part_2.py
@@ -1,8 +1,21 @@
 import sys
 
+def contains_repeating_letters(line: str) -> bool:
+    _c: int = 0
+    _str: str = ''
+    for i in range(len(line) - 2):
+        if line[i] + line[i + 1] == _str:
+            _c += 1
+    return
+
 def main() -> None:
     if len(sys.argv) < 2:
         print("ERROR: MISSING INPUT FILE ARGUMENT!")
         return
+    with open(sys.argv[1]) as _f:
+        for _line in _f:
+            break
+        _f.close()
+    return
 
 main()
