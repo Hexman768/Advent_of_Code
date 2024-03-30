ba9a13c389eff7b7638cf9784befc08e58c14608
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-03-30 13:54:50 -0500

    Added partial solution for part 2

--- 2023/day_1/main.py
+++ 2023/day_1/main.py
@@ -1,11 +1,7 @@
 import sys
 
-def main() -> None:
+def part_1() -> None:
     _sum: int = 0
-    if len(sys.argv) < 2:
-        print('ERROR: MISSING INPUT FILE ARGUMENT!')
-        return
-    
     with open(sys.argv[1]) as _file:
         for line in _file.readlines():
             _n_str: str = ''
@@ -19,5 +15,27 @@ def main() -> None:
             print(_n_str)
             print(f'Sum is {_sum}')
         _file.close()
+
+def part_2() -> None:
+    _sum: int = 0
+    _n_line_lst: list[str] = []
+    _num_vect: dict = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    with open(sys.argv[1]) as _file:
+        for line in _file.readlines():
+            for key in _num_vect.keys():
+                print(f'Checking if {key} in {line}')
+                if key in line:
+                    line = line.replace(key, _num_vect.get(key))
+            _n_line_lst.append(line)
+            print(line)
+        _file.close()
+    return
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print('ERROR: MISSING INPUT FILE ARGUMENT!')
+        return
+    part_2()
+    return
 
 main()
