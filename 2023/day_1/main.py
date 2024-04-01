aa93caf2f96cc65cb742a315599f4c728d84a5ff
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2024-04-01 10:57:39 -0500

    Added solution for Day 1 Part 2 of 2023 Advent of Code  (#7)
    * Added changes for day 1 puzzle
    
    * Added partial solution for part 2
    
    * Added more changes for efficiency
    
    * Added more efficient check for letter being an integer
    
    * Completed solution for part 2

--- 2023/day_1/main.py
+++ 2023/day_1/main.py
@@ -1,23 +1,43 @@
 import sys
 
-def main() -> None:
+def part_1(lines: list[str]) -> None:
     _sum: int = 0
+    for line in lines:
+        _n_str: str = ''
+        for i in range(len(line)):
+            if line[i].isdigit():
+                _n_str += line[i]
+        _n_str = _n_str[0] + _n_str[len(_n_str) - 1]
+        _sum += int(_n_str)
+    print(f'Sum is {_sum}')
+
+def part_2(lines: list[str]) -> None:
+    _n_line_lst: list[str] = []
+    _num_vect: dict = {'one':'o1e',
+                       'two':'t2o',
+                       'three':'t3e',
+                       'four':'f4r',
+                       'five':'f5e',
+                       'six':'s6x',
+                       'seven':'s7n',
+                       'eight':'e8t',
+                       'nine':'n9e'}
+    for line in lines:
+        for key in _num_vect.keys():
+            line = line.replace(key, _num_vect.get(key))
+        _n_line_lst.append(line)
+    part_1(_n_line_lst)
+    return
+
+def main() -> None:
     if len(sys.argv) < 2:
         print('ERROR: MISSING INPUT FILE ARGUMENT!')
         return
-    
-    with open(sys.argv[1]) as _file:
-        for line in _file.readlines():
-            _n_str: str = ''
-            #print(line)
-            for i in range(len(line)):
-                if line[i] in '0123456789':
-                    _n_str += line[i]
-            #print(_n_str)
-            _n_str = _n_str[0] + _n_str[len(_n_str) - 1]
-            _sum += int(_n_str)
-            print(_n_str)
-            print(f'Sum is {_sum}')
-        _file.close()
+    _file = open(sys.argv[1])
+    _lines: list[str] = _file.readlines()
+    _file.close()
+    part_1(_lines)
+    part_2(_lines)
+    return
 
 main()
