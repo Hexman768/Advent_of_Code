7fd5864dabfc043c1140fb02a32e88db127ea801
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-01 10:49:50 -0500

    Completed solution for part 2

--- 2023/day_1/main.py
+++ 2023/day_1/main.py
@@ -4,28 +4,29 @@ def part_1(lines: list[str]) -> None:
     _sum: int = 0
     for line in lines:
         _n_str: str = ''
-        #print(line)
         for i in range(len(line)):
             if line[i].isdigit():
                 _n_str += line[i]
-        #print(_n_str)
         _n_str = _n_str[0] + _n_str[len(_n_str) - 1]
         _sum += int(_n_str)
-        #print(_n_str)
-        print(f'Sum is {_sum}')
+    print(f'Sum is {_sum}')
 
 def part_2(lines: list[str]) -> None:
-    _sum: int = 0
     _n_line_lst: list[str] = []
-    _num_vect: dict = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    _num_vect: dict = {'one':'o1e',
+                       'two':'t2o',
+                       'three':'t3e',
+                       'four':'f4r',
+                       'five':'f5e',
+                       'six':'s6x',
+                       'seven':'s7n',
+                       'eight':'e8t',
+                       'nine':'n9e'}
     for line in lines:
         for key in _num_vect.keys():
-            print(f'Checking if {key} in {line}')
-            if key in line:
-                line = line.replace(key, _num_vect.get(key))
+            line = line.replace(key, _num_vect.get(key))
         _n_line_lst.append(line)
-        print(_n_line_lst)
-        part_1(_n_line_lst)
+    part_1(_n_line_lst)
     return
 
 def main() -> None:
@@ -36,7 +37,7 @@ def main() -> None:
     _lines: list[str] = _file.readlines()
     _file.close()
     part_1(_lines)
-    #part_2(_lines)
+    part_2(_lines)
     return
 
 main()
