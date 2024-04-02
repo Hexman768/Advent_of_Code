396e05a7e73c8ca89a829ae25ffa58ef967d8e45
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-02 13:21:20 -0500

    Added solution for part 2 of day 5

--- 2015/day_5/part_2.py
+++ 2015/day_5/part_2.py
@@ -1,19 +1,15 @@
+import re
 import sys
-
-def contains_repeating_letters(line: str) -> bool:
-    _c: int = 0
-    _str: str = ''
-    for i in range(len(line) - 2):
-        if line[i] + line[i + 1] == _str:
-            _c += 1
 
 def main() -> None:
     if len(sys.argv) < 2:
         print("ERROR: MISSING INPUT FILE ARGUMENT!")
         return
+    _c: int = 0
     with open(sys.argv[1], encoding='utf-8') as _f:
-        for _line in _f:
-            break
+        for _line in _f.readlines():
+            _c = _c + 1 if len(re.findall(r'([a-z]{2}).*\1', _line)) > 0 and len(re.findall(r'([a-z]).\1', _line)) > 0 else _c
         _f.close()
+    print(f'There are {_c} nice strings')
 
 main()
