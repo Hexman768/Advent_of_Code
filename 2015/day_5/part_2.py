15d93d966a8996f1ef19afdf1cf8b34d007da6d0
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-02 13:28:23 -0500

    Fixed Pylint line length issue

--- 2015/day_5/part_2.py
+++ 2015/day_5/part_2.py
@@ -8,7 +8,9 @@ def main() -> None:
     _c: int = 0
     with open(sys.argv[1], encoding='utf-8') as _f:
         for _line in _f.readlines():
-            _c = _c + 1 if len(re.findall(r'([a-z]{2}).*\1', _line)) > 0 and len(re.findall(r'([a-z]).\1', _line)) > 0 else _c
+            if (len(re.findall(r'([a-z]{2}).*\1', _line)) > 0 and 
+                len(re.findall(r'([a-z]).\1', _line)) > 0):
+                _c += 1
         _f.close()
     print(f'There are {_c} nice strings')
 
