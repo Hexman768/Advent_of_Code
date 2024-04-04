a71f1f407ea1cc21c45c674ffe86af4e5f1cb820
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-04 13:23:35 -0500

    Added more changes for part 1

--- 2015/day_6/main.py
+++ 2015/day_6/main.py
@@ -1,19 +1,32 @@
 import re
+import ast
 import sys
 
-def part_1(_lines: str) -> None:
+def extract_coords(coords_str: list[str]) -> None:
+    x1,y1,x2,y2 = 0,0,0,0
+    temp = tuple([x for x in coords_str])
+    #print(tuple([a for a in temp]))
+    return
+
+def part_1(lines: str) -> None:
     _inst: str = ''
     _min_xy: list[int] = []
     _max_xy: list[int] = []
-    for _line in _lines:
+    _coords: list[str] = []
+    _lights: list[int] = [[0 for i in range(1000)] for j in range(1000)]
+    for _line in lines:
         if 'on' in _line:
             _inst = 'on'
         elif 'off' in _line:
             _inst = 'off'
         else:
             _inst = 'toggle'
-        print(re.findall(r'(\d+[,]\d+)', _line))
-        print(_inst)
+        _coords = re.findall(r'(\d+[,]\d+)', _line)
+        _min_xy = ast.literal_eval(_coords[0])
+        _max_xy = ast.literal_eval(_coords[1])
+        print(_min_xy)
+        print(_max_xy)
+    extract_coords(_coords)
     return
 
 def main() -> None:
