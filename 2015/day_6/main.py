91a21da3140a56a8b78a09d843433cf581c1b352
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-05 10:02:55 -0500

    Added solution for part 2

--- 2015/day_6/main.py
+++ 2015/day_6/main.py
@@ -2,7 +2,7 @@ import re
 import ast
 import sys
 
-def part_1(lines: str) -> None:
+def execute(lines: str, bright: bool) -> None:
     _inst: str = ''
     _count: int = 0
     _min_xy: tuple[int] = []
@@ -19,30 +19,33 @@ def part_1(lines: str) -> None:
         _coords = re.findall(r'(\d+[,]\d+)', _line)
         _min_xy = ast.literal_eval(_coords[0])
         _max_xy = ast.literal_eval(_coords[1])
-        print(_min_xy)
-        print(_max_xy)
         for y in range(_min_xy[1], _max_xy[1] + 1):
             for x in range(_min_xy[0], _max_xy[0] + 1):
-                #print(f'X: {x} Y: {y}')
-                if _inst == 'on':
+                if _inst == 'on' and not bright:
                     _lights[y][x] = 1
-                elif _inst == 'off':
+                elif _inst == 'on' and bright:
+                    _lights[y][x] += 1
+                elif _inst == 'off' and not bright:
                     _lights[y][x] = 0
-                else:
+                elif _inst == 'off' and bright and _lights[y][x] > 0:
+                    _lights[y][x] -= 1
+                elif _inst == 'toggle' and not bright:
                     _lights[y][x] = 1 if _lights[y][x] == 0 else 0
+                elif _inst == 'toggle' and bright:
+                    _lights[y][x] += 2
     for i in range(1000):
         for j in range(1000):
-            if _lights[i][j] == 1:
-                _count += 1
-    print(f'There are {_count} lights turned on currently')
+            _count += _lights[i][j]
+    print(f'The total count is: {_count}')
 
 def main() -> None:
     if len(sys.argv) < 2:
-        print('[ERROR] Usage: main.py <input file path>')
+        print('[ERROR] Usage: py main.py <input file path>')
         return
     with open(sys.argv[1], encoding='utf-8') as _file:
         _lines: str = _file.readlines()
-        part_1(_lines)
+        execute(_lines, False)
+        execute(_lines, True)
         _file.close()
 
 main()
