58af991a1310cc072f48e84dd174a4ab08af6c5f
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-11 17:55:43 -0500

    Added solution for part 1

--- 2015/day_7/main.py
+++ 2015/day_7/main.py
@@ -1,30 +1,34 @@
 import sys
+from functools import lru_cache
 
 lines = []
 wires = {}
 
-def get_signal(key: str) -> int:
+@lru_cache
+def get_signal(k: str) -> int:
     try:
-        return int(key)
+        return int(k)
     except ValueError:
         pass
-    command = wires[key].strip().split(' ')
-    print(wires)
-    print(command)
+    command = wires[k].strip().split(' ')
+    #print(wires)
+    #print(command)
 
     if 'NOT' in command:
-        print(command)
-        return ~get_signal(command[1])
+        #print(command)
+        _temp = get_signal(command[1])
+        #print(_temp)
+        return (~_temp) & 0xffff
     elif 'AND' in command:
         return get_signal(command[0]) & get_signal(command[2])
     elif 'OR' in command:
         return get_signal(command[0]) | get_signal(command[2])
     elif 'LSHIFT' in command:
-        return get_signal(command[0]) << get_signal(command[2])
+        return (get_signal(command[0]) << get_signal(command[2])) & 0xffff
     elif 'RSHIFT' in command:
-        return get_signal(command[0]) >> get_signal(command[2])
+        return (get_signal(command[0]) >> get_signal(command[2])) & 0xffff
     else:
-        return command[0]
+        return get_signal(command[0])
 
 if len(sys.argv) < 2:
     print('[ERROR] Usage: py main.py <input file path>')
@@ -34,6 +38,6 @@ with open(sys.argv[1], encoding='utf-8') as _file:
     for line in lines:
         inst, key = line.split('->')
         wires[key.strip()] = inst.strip()
-    print(get_signal('i'))
+    print(get_signal('a'))
     #print(wires)
     _file.close()
