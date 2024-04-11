9d5ff0db77e9f1309b4ab097e874f09d722502f6
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-04-11 16:14:06 -0500

    Added almost completed solution to part1

--- 2015/day_7/main.py
+++ 2015/day_7/main.py
@@ -14,16 +14,29 @@ def part_1(lines: list[str]):
         (_i, _v) = line.split('->')
         _inst = _i.strip().split(' ')
         _var = _v.strip()
-        print((_inst, _var))
+        #print((_inst, _var))
         if len(_inst) == 1:
-            _vars[_var] = _inst
+            _vars[_var] = int(_inst[0])
         else:
             for k,v in _com.items():
                 if k in line:
                     if len(_inst) > 2:
                         print(f'{_var} = {_inst[0]}{v}{_inst[2]}')
+                        if _inst[1] == 'AND':
+                            _vars[_var] = _vars[_inst[0]] & _vars[_inst[2]]
+                            #print(_vars[_var])
+                        elif _inst[1] == 'OR':
+                            _vars[_var] = _vars[_inst[0]] | _vars[_inst[2]]
+                        elif _inst[1] == 'LSHIFT':
+                            print(_var)
+                            _vars[_var] = (_vars[_inst[0]] << int(_inst[2])) & 0xffff
+                        elif _inst[1] == 'RSHIFT':
+                            print(_var)
+                            _vars[_var] = _vars[_inst[0]] >> int(_inst[2]) & 0xffff
                     else:
-                        print(f'{_var} = {v}{_inst[1]}')
+                        #print(f'{_var} = {v}{_inst[1]}')
+                        _vars[_var] = (~_vars[_inst[1]]) & 0xffff
+    print(_vars)
 
 def main() -> None:
     if len(argv) < 2:
